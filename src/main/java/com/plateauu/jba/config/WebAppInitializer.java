dc6e11299568aaf73272ca8057a974efbcdc4bb6
Marcin Michalak <plateauu@o2.pl>
2016-12-26 01:40:43 +0100

    some refactor

--- src/main/java/com/plateauu/jba/config/WebAppInitializer.java
+++ src/main/java/com/plateauu/jba/config/WebAppInitializer.java
@@ -13,26 +13,17 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
-
-    public static final String DEV = "dev";
-    public static final String PROD = "prod";
-
-    private final Logger log = Logger.getLogger(getClass().getName());
-
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "prod");
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
 
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, PROD);
-
-//        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, DEV);
-
         rootContext.register(RootContext.class, RootContextDev.class, WebSecurityConfig.class);
 
         servletContext.addListener(new ContextLoaderListener(rootContext));
@@ -52,14 +43,6 @@ public class WebAppInitializer implements WebApplicationInitializer {
         encoding.setInitParameters(params);
 
         encoding.addMappingForUrlPatterns(null, true, "/*");
-
-
-//        OpenEntityManagerInViewFilter entityManagerInViewFilter = new OpenEntityManagerInViewFilter();
-//        entityManagerInViewFilter.setEntityManagerFactoryBeanName("emf");
-//        entityManagerInViewFilter.setPersistenceUnitName("transactionManager");
-//
-//        FilterRegistration.Dynamic filter = servletContext.addFilter("ENTITY_MANAGER_FILTER_NAME", entityManagerInViewFilter);
-//        filter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/*");
 
 
     }
